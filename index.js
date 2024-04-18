7620ad759771f9321e787f1e5ea5488cff2e399a
Keira Pierce <kpierce1@samford.edu>
2024-04-18 03:27:33 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const writeCards = (names,event) => {
+    const messages = []
+    for(let i = 0; i < names.length;i++) {
+        
+           const greetingMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+          
+    }
+    return messages
+
+}
+
+function countDown() {
+    for (let i = 10; i >= 0; i--) {
+      console.log(i);
+    }
+  }
